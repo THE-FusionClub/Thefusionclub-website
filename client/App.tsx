52346f05aad7017a5625cc7578ae4488ba184ac7
shreya-meaw <shreyasingh7297@gmail.com>
2026-07-05 02:09:33 +0530

    update

--- client/App.tsx
+++ client/App.tsx
@@ -34,6 +34,7 @@ const App = () => (
           <Route path="/signup" element={<SignUp />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
